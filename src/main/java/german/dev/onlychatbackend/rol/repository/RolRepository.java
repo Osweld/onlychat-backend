e88216b6b2bc660ef0e17cbc5f54a2a982261820
German Reyes <oswaldo101.gr@gmail.com>
2025-02-20 12:42:26 -0600

    Remove unused import and clean up RolRepository interface

--- src/main/java/german/dev/onlychatbackend/rol/repository/RolRepository.java
+++ src/main/java/german/dev/onlychatbackend/rol/repository/RolRepository.java
@@ -3,8 +3,9 @@ package german.dev.onlychatbackend.rol.repository;
 import german.dev.onlychatbackend.rol.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+
 
 public interface RolRepository extends JpaRepository<Rol, Long> {
-    Optional<Rol> findByRol(String rol);
+
+
 }
