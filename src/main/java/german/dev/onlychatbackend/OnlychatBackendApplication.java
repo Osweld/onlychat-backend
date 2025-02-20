146f6be406fffc2226f940a8191318d06136baa2
German Reyes <oswaldo101.gr@gmail.com>
2025-02-20 10:30:49 -0600

    Initial commit

---
+++ src/main/java/german/dev/onlychatbackend/OnlychatBackendApplication.java
@@ -0,0 +1,13 @@
+package german.dev.onlychatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlychatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlychatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/german/dev/onlychatbackend/OnlychatBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package german.dev.onlychatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlychatBackendApplicationTests {
+
+
+
+}
