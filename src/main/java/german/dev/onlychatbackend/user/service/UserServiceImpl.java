57507ea8b39d1c82c5be891c87dc1e7846ef0eb4
German Reyes <oswaldo101.gr@gmail.com>
2025-02-21 17:59:34 -0600

    Implement user management methods in UserServiceImpl for saving, updating, and deleting users

--- src/main/java/german/dev/onlychatbackend/user/service/UserServiceImpl.java
+++ src/main/java/german/dev/onlychatbackend/user/service/UserServiceImpl.java
@@ -5,6 +5,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import german.dev.onlychatbackend.rol.entity.Rol;
+import german.dev.onlychatbackend.rol.enums.RolEnum;
+import german.dev.onlychatbackend.user.dto.UserRequestDTO;
+import german.dev.onlychatbackend.user.dto.UserResponseDTO;
+import german.dev.onlychatbackend.user.dto.UserUpdateDTO;
+import german.dev.onlychatbackend.user.entity.User;
+import german.dev.onlychatbackend.user.entity.UserStatus;
+import german.dev.onlychatbackend.user.enums.UserStatusEnum;
+import german.dev.onlychatbackend.user.mapper.UserMapper;
 import german.dev.onlychatbackend.user.projection.UserProjection;
 import german.dev.onlychatbackend.user.repository.UserRepository;
 
@@ -12,9 +21,11 @@ import german.dev.onlychatbackend.user.repository.UserRepository;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -39,6 +50,31 @@ public class UserServiceImpl implements UserService {
     @Transactional(readOnly = true)
     public Page<UserProjection> findAllUsers(Pageable pageable) {
         return userRepository.findAllUsers(pageable);
+    }
+
+    @Override
+    @Transactional
+    public UserResponseDTO saveUser(UserRequestDTO userRequestDTO) {
+        User user = userMapper.toEntity(userRequestDTO);
+        user.setRol(new Rol(Long.valueOf(RolEnum.USER.getId())));
+        user.setUserStatus(new UserStatus(Long.valueOf(UserStatusEnum.INACTIVE.getId())));
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    @Override
+    @Transactional
+    public UserResponseDTO updateUser(Long id, UserUpdateDTO userUpdateDTO) {
+        User userToUpdate = userRepository.findById(id).orElseThrow();
+        userMapper.updateEntity(userToUpdate, userUpdateDTO);
+        return userMapper.toDto(userRepository.save(userToUpdate));
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        User userToDelete = userRepository.findById(id).orElseThrow();
+        userToDelete.setUserStatus(new UserStatus(Long.valueOf(UserStatusEnum.DELETED.getId())));
+        userRepository.save(userToDelete);
     }
 
 }
