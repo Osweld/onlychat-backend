28de1a6591ab1157ea4063d19acedcd927a06a90
German Reyes <oswaldo101.gr@gmail.com>
2025-02-21 17:59:29 -0600

    Add methods for user management in UserService interface

--- src/main/java/german/dev/onlychatbackend/user/service/UserService.java
+++ src/main/java/german/dev/onlychatbackend/user/service/UserService.java
@@ -3,6 +3,9 @@ package german.dev.onlychatbackend.user.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import german.dev.onlychatbackend.user.dto.UserRequestDTO;
+import german.dev.onlychatbackend.user.dto.UserResponseDTO;
+import german.dev.onlychatbackend.user.dto.UserUpdateDTO;
 import german.dev.onlychatbackend.user.projection.UserProjection;
 
 public interface UserService {
@@ -11,5 +14,10 @@ public interface UserService {
     UserProjection findUserbyUsername(String username);
     Page<UserProjection> searchUserByUsername(String username, Pageable pageable);
     Page<UserProjection> findAllUsers(Pageable pageable);
+    UserResponseDTO saveUser(UserRequestDTO userRequestDTO);
+    UserResponseDTO updateUser(Long id, UserUpdateDTO userUpdateDTO);
+    void deleteUser(Long id);
+
+    
 
 }
