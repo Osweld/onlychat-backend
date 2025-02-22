d0586d63ddcf877f516cf8e505bd966786a624f2
German Reyes <oswaldo101.gr@gmail.com>
2025-02-21 18:08:14 -0600

    Add user management endpoints in UserController for saving, updating, and deleting users

--- src/main/java/german/dev/onlychatbackend/user/controller/UserController.java
+++ src/main/java/german/dev/onlychatbackend/user/controller/UserController.java
@@ -4,14 +4,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import german.dev.onlychatbackend.user.dto.UserRequestDTO;
+import german.dev.onlychatbackend.user.dto.UserResponseDTO;
+import german.dev.onlychatbackend.user.dto.UserUpdateDTO;
 import german.dev.onlychatbackend.user.projection.UserProjection;
 import german.dev.onlychatbackend.user.service.UserService;
+import jakarta.validation.Valid;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
@@ -39,10 +50,27 @@ public class UserController {
         return ResponseEntity.ok(userService.searchUserByUsername(username,PageRequest.of(page, size)));
     }
 
-    @GetMapping("")
+    @GetMapping()
     ResponseEntity<Page<UserProjection>> getAllUsers (@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size) {
         return ResponseEntity.ok(userService.findAllUsers(PageRequest.of(page, size)));
     }
+
+    @PostMapping()
+    public ResponseEntity<UserResponseDTO> saveUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
+       return new ResponseEntity<>(userService.saveUser(userRequestDTO), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO userUpdateDTO) {
+        return new ResponseEntity<>(userService.updateUser(id, userUpdateDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
     
 
 }
