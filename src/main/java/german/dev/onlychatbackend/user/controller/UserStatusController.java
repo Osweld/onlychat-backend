0cc36fc3bcc98fdff15f5db6137c7b043c4ccd0f
German Reyes <oswaldo101.gr@gmail.com>
2025-02-23 20:37:45 -0600

    Add role-based access control to findAll method in UserStatusController

--- src/main/java/german/dev/onlychatbackend/user/controller/UserStatusController.java
+++ src/main/java/german/dev/onlychatbackend/user/controller/UserStatusController.java
@@ -3,6 +3,7 @@ package german.dev.onlychatbackend.user.controller;
 import german.dev.onlychatbackend.user.entity.UserStatus;
 import german.dev.onlychatbackend.user.service.UserStatusService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class UserStatusController {
     }
 
     @GetMapping
+    @Secured("ROLE_ADMIN")
     ResponseEntity<List<UserStatus>> findAll(){
         return ResponseEntity.ok(userStatusService.findAll());
     }
