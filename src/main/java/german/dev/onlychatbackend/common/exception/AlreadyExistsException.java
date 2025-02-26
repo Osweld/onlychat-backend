73656a42ab2ad3515f73d0a296354950ea4d3ab0
German Reyes <oswaldo101.gr@gmail.com>
2025-02-26 15:08:16 -0600

    Add AlreadyExistsException for handling resource already exists scenarios

---
+++ src/main/java/german/dev/onlychatbackend/common/exception/AlreadyExistsException.java
@@ -0,0 +1,13 @@
+package german.dev.onlychatbackend.common.exception;
+
+public class AlreadyExistsException  extends RuntimeException{
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public AlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
