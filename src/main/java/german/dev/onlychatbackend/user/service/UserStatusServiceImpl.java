3aeec029958ad3aab304dd8d521b374ced38ed57
German Reyes <oswaldo101.gr@gmail.com>
2025-02-26 15:09:30 -0600

    Add UserStatusNotFoundException handling in findAll method of UserStatusServiceImpl

--- src/main/java/german/dev/onlychatbackend/user/service/UserStatusServiceImpl.java
+++ src/main/java/german/dev/onlychatbackend/user/service/UserStatusServiceImpl.java
@@ -1,6 +1,7 @@
 package german.dev.onlychatbackend.user.service;
 
 import german.dev.onlychatbackend.user.entity.UserStatus;
+import german.dev.onlychatbackend.user.exception.UserStatusNotFoundException;
 import german.dev.onlychatbackend.user.repository.UserStatusRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,11 @@ public class UserStatusServiceImpl implements UserStatusService {
     @Override
     @Transactional(readOnly = true)
     public List<UserStatus> findAll() {
-        return userStatusRepository.findAll();
+        try{
+            return userStatusRepository.findAll();
+        }catch (Exception e){
+            throw new UserStatusNotFoundException("Error while fetching user status");
+        }
+        
     }
 }
