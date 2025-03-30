59a6433db7635185e1e4eee3108a92f23064f889
German Reyes <oswaldo101.gr@gmail.com>
2025-03-29 20:34:22 -0600

    Add validation annotations for email in ActivateAccountResponseDTO

--- src/main/java/german/dev/onlychatbackend/auth/dto/ActivateAccountResponseDTO.java
+++ src/main/java/german/dev/onlychatbackend/auth/dto/ActivateAccountResponseDTO.java
@@ -1,5 +1,7 @@
 package german.dev.onlychatbackend.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -7,6 +9,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class ActivateAccountResponseDTO {
 
+    @NotBlank(message = "Email cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Email is not valid")
     private String email;
 
 }
