f130020ac37173c31028fb8a3164e14f91d43170
German Reyes <oswaldo101.gr@gmail.com>
2025-04-22 20:40:46 -0600

    Refactor ChatMapper to improve attribute naming and include joinedAt in CreatePersonalChatResponseDTO

--- src/main/java/german/dev/onlychatbackend/chat/mapper/ChatMapper.java
+++ src/main/java/german/dev/onlychatbackend/chat/mapper/ChatMapper.java
@@ -1,5 +1,7 @@
 package german.dev.onlychatbackend.chat.mapper;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Component;
 
 import german.dev.onlychatbackend.chat.dto.CreatePersonalChatResponseDTO;
@@ -8,13 +10,13 @@ import german.dev.onlychatbackend.chat.dto.CreatePersonalChatResponseDTO;
 public class ChatMapper {
 
 
-    public CreatePersonalChatResponseDTO toCreatePersonalChatResponseDTO(Long chatId, String chatName, String chatType, String username, String otherUsername) {
+    public CreatePersonalChatResponseDTO toCreatePersonalChatResponseDTO(Long chatId, String chatName, String chatType, String otherUsername, LocalDateTime joinedAt) {
         CreatePersonalChatResponseDTO dto = new CreatePersonalChatResponseDTO();
-        dto.setChatId(chatId);
-        dto.setChatName(chatName);
+        dto.setId(chatId);
+        dto.setName(chatName);
         dto.setChatType(chatType);
-        dto.setUsername(username);
         dto.setOtherUsername(otherUsername);
+        dto.setJoinedAt(joinedAt);
         return dto;
     }
 
